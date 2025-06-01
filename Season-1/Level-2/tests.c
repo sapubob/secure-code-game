0ddb554682bbd6155163983d52f90f1e67aefd55
sapubob <sapubob@gmail.com>
2025-06-01 11:45:58 -0500

    Update tests.c

--- Season-1/Level-2/tests.c
+++ Season-1/Level-2/tests.c
@@ -11,7 +11,7 @@ int main() {
     printf("Level 2 \n\n");
     // Creates a non-admin username called "pwned"
     int user1 = create_user_account(false, "pwned");
-    printf("0. Non-admin (admin:%i) username called '%s' has been created \n\n", is_admin(user1), username(user1));
+    printf("0. Non-admin (admin:%i) user %i named '%s' has been created \n\n", is_admin(user1), user1, username(user1));
 
     printf("1. Non-admin users like '%s' can update some dummy numerical settings \n", username(user1));
     printf("2. Non-admin users have no access to settings that can escalate themselves to admins \n\n"); 
