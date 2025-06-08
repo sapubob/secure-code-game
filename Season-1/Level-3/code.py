8fcc9faca005fd698b2ab70b9b4282b4945b479b
sapubob <sapubob@gmail.com>
2025-06-08 17:26:39 +0000

    Update code.py

--- Season-1/Level-3/code.py
+++ Season-1/Level-3/code.py
@@ -29,6 +29,7 @@ class TaxPayer:
         #if path.startswith('/') or path.startswith('..'):
         if path != os.path.normpath(path):
             return None
+        
         # builds path
         base_dir = os.path.dirname(os.path.abspath(__file__))
         #prof_picture_path = os.path.normpath(os.path.join(base_dir, path))
@@ -43,9 +44,11 @@ class TaxPayer:
         tax_data = None
         if not path:
             raise Exception("Error: Tax form is required for all users")
+        
         # defends against path traversal attacks
         if path != os.path.normpath(path):
             return None
+        
         with open(path, 'rb') as form:
             tax_data = bytearray(form.read())
         # assume that tax data is returned on screen after this
